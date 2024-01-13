9906cc874a4f1c53e335123156af2f00b411de74
Andrew Peterson <apeterson247@gmail.com>
2024-01-13 18:58:58 -0500

    Remove unused code

--- src/server/server.ts
+++ src/server/server.ts
@@ -19,24 +19,6 @@ app.use(cors(options));
 app.use(express.json());
 app.use(morgan("common"));
 
-/**
- * Error handler middleware for validation errors.
- */
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    console.log("MiddleWare");
-    // Check the error is a validation error
-    if (error instanceof ValidationError) {
-      // Handle the error
-      response.status(400).send(error.validationErrors);
-      next();
-    } else {
-      // Pass error on if not a validation error
-      next(error);
-    }
-  }
-);
-
 //Routes
 app.use("/api/drinks", drinksRouter);
 
