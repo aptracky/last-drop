6bcb8bb3e272abc4bc39aa6159494a6fc331324a
Andrew Peterson <apeterson247@gmail.com>
2024-01-14 14:52:07 -0500

    Remove unused files

--- dist/server/utils/dynamodb.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dynamoClient = void 0;
-const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
-const region = process.env.REGION;
-const client = new client_dynamodb_1.DynamoDBClient({ region: region });
-exports.dynamoClient = client;
