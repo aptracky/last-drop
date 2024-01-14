9d43718d4672358e058d7f058398469a534a245b
Andrew Peterson <apeterson247@gmail.com>
2024-01-14 17:58:42 -0500

    Update to use env vars

--- src/server/utils/dynamodb.ts
+++ src/server/utils/dynamodb.ts
@@ -1,7 +1,12 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-const region = process.env.REGION;
+const region = process.env.REGION || "us-east-1";
+const accessKeyId = process.env.AWS_KEY || "";
+const secretAccessKey = process.env.AWS_SECRET || "";
 
-const client = new DynamoDBClient({ region: region });
+const client = new DynamoDBClient({
+  credentials: { accessKeyId: accessKeyId, secretAccessKey: secretAccessKey },
+  region: region,
+});
 
 export { client as dynamoClient };
